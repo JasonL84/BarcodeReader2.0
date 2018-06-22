69db1491951c3f0e0ce06dec476afa751d3596ed
DESKTOP-NUER16T\liuja <liu.jason84@gmail.com>
2018-06-22 17:46:53 -0400

    Updated with UI and can now save options into config file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
 import gnu.io.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
 import java.security.GeneralSecurityException;
@@ -8,6 +11,16 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.text.JTextComponent;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
@@ -32,9 +45,12 @@ class Book implements Comparable{
 	public Book(Object[] args) {
 		id = args[0].toString();
 		name = args[1].toString();
-		dateOut = args[2].toString();
-		studentNum = args[3].toString();
-		if(args[4].equals("Yes")) signedOut = true;
+		if(args.length>2) {
+			dateOut = args[2].toString();
+			studentNum = args[3].toString();
+			if(args[4].equals("Yes")) signedOut = true;
+			else signedOut = false;
+		}
 		else signedOut = false;
 	}
 	@Override
@@ -67,6 +83,9 @@ public class Main {
 	private static final String CLIENT_SECRET_DIR = "client_secret.json";
 	private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
 	private static ArrayList<Book> books = new ArrayList<Book>();
+	private static Sheets service;
+	public static boolean doneLoading = false;
+	public static boolean isClose = false;
 	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
 		// Load client secrets.
 		InputStream in = Main.class.getResourceAsStream(CLIENT_SECRET_DIR);
@@ -85,126 +104,179 @@ public class Main {
 		return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
 	}
 	private static void init(Sheets s) throws IOException {
-		final String RANGE = "Sheet1!A2:E";
-		List<List<Object>> values = s.spreadsheets().values().get("1cAj66vchiSpRpbq47uwMjejiRodvVtQUCV0umbbogT0",RANGE).execute().getValues();
+		final String RANGE = "Test!B5:G";
+		List<List<Object>> values = s.spreadsheets().values().get("1OArA90Lt5Rgt169X8gk4wvQSmqvzE0PKD1KdNmTxY4o",RANGE).execute().getValues();
 		for(List l:values) {
 			books.add(new Book(l.toArray()));
 		}
-	}
-	private static void close(Sheets s,List<List<Object>> values) throws IOException{
-		final String spreadsheetId = "1cAj66vchiSpRpbq47uwMjejiRodvVtQUCV0umbbogT0";
-		final String range = "Sheet1!A2:E";
-        ValueRange body = new ValueRange().setValues(values);
-		UpdateValuesResponse response = s.spreadsheets().values().update(spreadsheetId,range,body).setValueInputOption("RAW")
-        		.execute();
-        System.out.println("Success");
-	}
-	public static void main(String[] args) throws PortInUseException, IOException, GeneralSecurityException  {		
-		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-		final String spreadsheetId = "1cAj66vchiSpRpbq47uwMjejiRodvVtQUCV0umbbogT0";
-		final String range = "Sheet1!A2:E";
-		Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
-				.setApplicationName(APPLICATION_NAME)
-				.build();
-		init(service);
 		System.out.println("Current Books are:");
 		for(Book b:books) System.out.println(b);
+	}
+	public static String getBook(String bookNum) {
+		for(Book b:books) {
+			if(b.id.equals(bookNum)) {
+				return b.name;
+			}
+		}
+		return null;
+	}
+	public static void close() throws IOException{
+		final String spreadsheetId = "1OArA90Lt5Rgt169X8gk4wvQSmqvzE0PKD1KdNmTxY4o";
+		final String range = "Test!B5:G";
 		List<List<Object>> values = new ArrayList<List<Object>>();
 		for(Book b:books) {
 			values.add(b.toList());
 		}
-		close(service,values);
-		// Build a new authorized API client service.
+		ValueRange body = new ValueRange().setValues(values);
+		UpdateValuesResponse response = service.spreadsheets().values().update(spreadsheetId,range,body).setValueInputOption("RAW")
+				.execute();
 
-		/* List<List<Object>> values = Arrays.asList(Arrays.asList(
-        		"Test","Test2"
-
-
-        		));
-        ValueRange body = new ValueRange().setValues(values);
-        UpdateValuesResponse response = service.spreadsheets().values().update(spreadsheetId,range,body).setValueInputOption("RAW")
-        		.execute();
-        System.out.println("Success");*/
+		System.out.println("Success");
+		System.exit(0);
 	}
-	private static void registerBook(int n,String name,String bookId) throws IOException,PortInUseException{
-		RXTXPort c = new RXTXPort("COM3") ;
-		DataInputStream br = new DataInputStream(c.getInputStream());
-		for(int i = 0; i<n; i++) {
+	public static void main(String[] args) throws PortInUseException, IOException, GeneralSecurityException  {		
+
+		final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+		final String spreadsheetId = "1OArA90Lt5Rgt169X8gk4wvQSmqvzE0PKD1KdNmTxY4o";
+		service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+				.setApplicationName(APPLICATION_NAME)
+				.build();
+		InputStream in = Main.class.getResourceAsStream("options.properties");
+		if(in == null) {
+			firstTimeSetup();
+			while (!doneLoading) {
+			}
+		}
+		System.out.println("First Time Setup complete");
+		final String range = "Test!A5:E";
+
+		init(service);
+		UIThread t = new UIThread();
+		t.start();
+		System.out.println("Init complete");
+	}
+	public static void registerBook(String name,String bookId) throws IOException,PortInUseException{
 			Object[] o = {bookId,name,"","",""};
+			for(Book b:books) {
+				if(b.id.equals(bookId)) {
+					System.out.println("Error book already exists");
+					return;
+				}
+			}
 			books.add(new Book(o));
 			System.out.println("Successfully added book");
-		}
 	}
-	private static void signIn(String bookId, String studentNum) throws Exception{
-		RXTXPort c = new RXTXPort("COM3") ;
-		DataInputStream br = new DataInputStream(c.getInputStream());
+	public static void signIn(String bookId, String studentNum) throws Exception{
 
-		while(true) {
-			String s = br.readLine();
-			if(s!=null) {
-				if(bookId == null) {
-					bookId = s;
+		boolean bookExists = false;
+		//Check if book is already checked out
+		for(Book b:books) {
+			if(bookId.equals(b.toString())) {
+				bookExists = true;
+				if(!b.signedOut) {
+					System.out.println("Error book already signed in");
 				}
 				else {
-					studentNum = s;
-					boolean bookExists = false;
-					//Check if book is already checked out
-					for(Book b:books) {
-						if(bookId.equals(b.toString())) {
-							bookExists = true;
-							if(!b.signedOut) {
-								System.out.println("Error book already signed in");
-							}
-							else {
-								b.dateOut = "";
-								b.studentNum = "";
-								b.signedOut = false;
-								System.out.println("Success, book has been signed in");
-							}
-						}
-
-					}
-					if(!bookExists) System.out.println("Error, cannot find book");
-					bookId = null;
-					studentNum = null;
+					b.dateOut = "";
+					b.studentNum = "";
+					b.signedOut = false;
+					System.out.println("Success, book has been signed in");
 				}
 			}
+
 		}
-			
+		if(!bookExists) System.out.println("Error, cannot find book");
+		bookId = null;
+		studentNum = null;
 	}
-	private static void signOut(String bookId, String studentNum) throws Exception{
-		RXTXPort c = new RXTXPort("COM3") ;
-		DataInputStream br = new DataInputStream(c.getInputStream());
-		while(true) {
-			String s = br.readLine();
-			if(s!=null) {
-				if(bookId == null) {
-					bookId = s;
+
+
+
+	public static void signOut(String bookId, String studentNum) throws Exception{
+		boolean bookExists = false;
+		//Check if book is already checked out
+		System.out.println("Seraching for book "+bookId);
+		for(Book b:books) {
+			System.out.println("Comparing to book "+b.toString());
+			if(bookId.equals(b.toString())) {
+				bookExists = true;
+				if(b.signedOut) {
+					System.out.println("Error book already signed out");
 				}
 				else {
-					studentNum = s;
-					boolean bookExists = false;
-					//Check if book is already checked out
-					for(Book b:books) {
-						if(bookId.equals(b.toString())) {
-							bookExists = true;
-							if(b.signedOut) {
-								System.out.println("Error book already signed out");
-							}
-							else {
-								b.dateOut = new Date().toString();
-								b.studentNum = studentNum;
-								b.signedOut = true;
-								System.out.println("Success, book has been signed out");
-							}
-						}
-
-					}
-					if(!bookExists) System.out.println("Error, cannot find book");
-					bookId = null;
-					studentNum = null;
+					b.dateOut = new Date().toString();
+					b.studentNum = studentNum;
+					b.signedOut = true;
+					System.out.println("Success, book has been signed out");
 				}
 			}
+
 		}
+		if(!bookExists) System.out.println("Error, cannot find book");
+		bookId = null;
+		studentNum = null;
+	}
+	public static void firstTimeSetup() throws IOException{
+		File f = new File(".\\src\\main\\resources\\options.properties");
+		f.createNewFile();
+		JFrame frame = new JFrame("First Time Setup");
+		JTextArea sheet = new JTextArea();
+		JLabel sheetLabel = new JLabel("Sheet ID");
+		List pa = Collections.list(CommPortIdentifier.getPortIdentifiers()); 
+		ArrayList<String> s = new ArrayList<String>();
+		for(Object o:pa) {
+			s.add(((CommPortIdentifier)o).getName());
+		}
+		JComboBox portCombo = new JComboBox(s.toArray());
+		JPanel p = new JPanel();
+		JLabel portLabel = new JLabel("Port Reader is attatched to");
+		JLabel title = new JLabel("First time setup");
+		JButton submit = new JButton("Done");
+		List l = new ArrayList<JComponent>(); 
+		l.add(sheet);
+		l.add(portCombo);
+		submit.addActionListener(new SubmitListener(frame,f,l));
+		p.setLayout(new BoxLayout(p,BoxLayout.PAGE_AXIS));
+		p.add(title);
+		p.add(sheetLabel);
+		p.add(sheet);
+		p.add(portLabel);
+		p.add(portCombo);
+		p.add(submit);
+		frame.add(p);
+		frame.pack();
+		frame.show();
+		System.out.println("Frame should be shown");
 	}
 }
+class SubmitListener implements ActionListener{
+	JFrame frame;
+	File file;
+	List<JTextComponent> comp;
+	String out = "";
+	public SubmitListener(JFrame f,File fil, List l) throws IOException {
+		frame = f;
+		file = fil;
+		comp = l;
+
+	}
+	@Override
+	public void actionPerformed(ActionEvent e){
+		Main.doneLoading = true;
+		for(JTextComponent c:comp) {
+			out.concat(c.getText());
+		}
+		BufferedWriter br;
+		try {
+			br = new BufferedWriter(new FileWriter(file));
+			br.write(out);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		frame.dispose();
+	}
+	
+}
+
+
---
+++ src/main/java/UIThread.java
@@ -0,0 +1,197 @@
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.DataInputStream;
+import java.io.IOException;
+
+import javax.swing.*;
+
+import gnu.io.PortInUseException;
+import gnu.io.RXTXPort;
+class PanelComp extends JPanel{
+	JComponent labelPanel = new JPanel();
+	JComponent textPanel = new JPanel();
+	JTextArea StudNumA = new JTextArea(0,36);
+	JTextArea BookNumA = new JTextArea(0,36);
+	JTextArea BookNameA = new JTextArea(0,36);
+	public PanelComp() {
+		textPanel.setLayout(new BoxLayout(textPanel,BoxLayout.Y_AXIS));
+		labelPanel.setLayout(new BoxLayout(labelPanel,BoxLayout.Y_AXIS));
+		labelPanel.add(new JLabel("Student Number"));
+		textPanel.add(StudNumA);
+		textPanel.add(BookNumA);
+		textPanel.add(BookNameA);
+		labelPanel.add(new JLabel("Book ID"));
+		labelPanel.add(new JLabel("Book Name"));
+		this.add(textPanel);
+		this.add(labelPanel);
+	}
+}
+public class UIThread extends Thread{
+	JButton SubmitCO = new JButton("Submit");
+	JButton SubmitCI = new JButton("Submit");
+	JButton SubmitReg = new JButton("Submit");
+	PanelComp checkOutPanel = new PanelComp();
+	PanelComp checkInPanel = new PanelComp();
+	PanelComp registerPanel = new PanelComp();
+	JTabbedPane pane = new JTabbedPane();
+	JFrame frame = new JFrame("Test");
+	public void start() {
+		SubmitCO.addActionListener(new SubmitListener("Check Out"));
+		checkOutPanel.add(SubmitCO,BorderLayout.SOUTH);
+		SubmitCI.addActionListener(new SubmitListener("Check In"));
+		checkInPanel.add(SubmitCI,BorderLayout.SOUTH);
+		SubmitReg.addActionListener(new SubmitListener("Register"));
+		registerPanel.add(SubmitReg,BorderLayout.SOUTH);
+		pane.addTab("Checkout", checkOutPanel);
+		pane.addTab("Check In", checkInPanel);
+		checkOutPanel.BookNameA.setEditable(false);
+		checkInPanel.BookNameA.setEditable(false);
+		pane.addTab("Register", registerPanel);
+		frame.add(pane);
+		frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new WindowClose());
+		frame.show();
+		frame.toFront();
+		frame.pack();
+		frame.repaint();
+		run();
+	}
+	public void run() {
+		RXTXPort c = null;
+		try {
+			c = new RXTXPort("COM3");
+		} catch (PortInUseException e) {
+			e.printStackTrace();
+		}
+		DataInputStream br = new DataInputStream(c.getInputStream());
+		String in = null;
+		while(true) {
+			if(pane.getSelectedIndex() == 0) {
+				String bookName;
+				bookName = Main.getBook(checkOutPanel.BookNumA.getText());
+				checkOutPanel.BookNameA.setText(bookName);
+			}
+			else if(pane.getSelectedIndex() == 1) {
+				String bookName;
+				bookName = Main.getBook(checkInPanel.BookNumA.getText());
+				checkInPanel.BookNameA.setText(bookName);
+			}
+			try {
+				in = br.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(in != null) {
+				System.out.println(in);
+				if(pane.getSelectedIndex()==0) {
+					if(checkOutPanel.StudNumA.getText().equals("")) {
+						checkOutPanel.StudNumA.setText(in);
+					}
+					else if(checkOutPanel.BookNumA.getText().equals("")) {
+						checkOutPanel.BookNumA.setText(in);
+					}
+				}
+			}
+			else if(pane.getSelectedIndex()==1) {
+				if(checkInPanel.StudNumA.getText().equals("")) {
+					checkInPanel.StudNumA.setText(in);
+				}
+				else if(checkInPanel.BookNumA.getText().equals("")) {
+					checkInPanel.BookNumA.setText(in);
+				}
+
+			}
+
+
+
+			frame.repaint();
+		}
+	}
+
+	private class SubmitListener implements ActionListener{
+		String type;
+		public SubmitListener(String type) {
+			this.type = type;
+		}
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+
+			try {
+				switch(type) {
+				case "Check Out":
+					Main.signOut(checkOutPanel.BookNumA.getText(), checkOutPanel.StudNumA.getText());
+					checkOutPanel.BookNumA.setText(null);
+					checkOutPanel.StudNumA.setText(null);
+				case "Check In": 
+					Main.signIn(checkInPanel.BookNumA.getText(), checkInPanel.StudNumA.getText());
+					checkInPanel.BookNumA.setText(null);
+					checkInPanel.StudNumA.setText(null);
+				case "Register": 
+					Main.registerBook(registerPanel.BookNameA.getText(), registerPanel.BookNumA.getText());
+					registerPanel.BookNumA.setText(null);
+					registerPanel.BookNameA.setText(null);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	class WindowClose implements WindowListener{
+
+		@Override
+		public void windowActivated(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void windowClosed(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void windowClosing(WindowEvent arg0) {
+			try {
+				Main.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			frame.dispose();
+		}
+
+		@Override
+		public void windowDeactivated(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void windowDeiconified(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void windowIconified(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void windowOpened(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+}
